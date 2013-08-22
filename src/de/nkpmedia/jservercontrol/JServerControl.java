673e663fd759d8f4a070ea24b9dc85c4ab58589d
paul <paul@pbnl.de>
2013-08-22 20:51:40 +0200

    Added a main class and a class with static functions for logging

---
+++ src/de/nkpmedia/jservercontrol/JServerControl.java
@@ -0,0 +1,12 @@
+package de.nkpmedia.jservercontrol;
+
+public class JServerControl
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/de/nkpmedia/jservercontrol/log/Log.java
@@ -0,0 +1,14 @@
+package de.nkpmedia.jservercontrol.log;
+
+public class Log
+{
+	static void log(String log){
+		System.out.println(log);
+	}
+	static void logError(String log){
+		System.out.println(log);
+	}
+	static void logMessage(String log){
+		System.out.println(log);
+	}
+}
